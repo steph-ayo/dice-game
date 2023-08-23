477cd1773b1bafab6578c336699dcdfd8da0b13e
steph-ayo <popoolastephen19@gmail.com>
2023-08-23 23:43:22 +0100

    scriptjs-update

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
-'use strict';
-const player0El = document.querySelector('.player--0');
-const player1El = document.querySelector('.player--1');
+"use strict";
+const player0El = document.querySelector(".player--0");
+const player1El = document.querySelector(".player--1");
 
-const score0El = document.querySelector('#score--0');
-const score1El = document.getElementById('score--1');
-const diceEl = document.querySelector('.dice');
-const btnNew = document.querySelector('.btn--new');
+const score0El = document.querySelector("#score--0");
+const score1El = document.getElementById("score--1");
+const diceEl = document.querySelector(".dice");
+const btnNew = document.querySelector(".btn--new");
 
-const btnRoll = document.querySelector('.btn--roll');
-const btnHold = document.querySelector('.btn--hold');
-const current0El = document.getElementById('current--0');
-const current1El = document.getElementById('current--1');
+const btnRoll = document.querySelector(".btn--roll");
+const btnHold = document.querySelector(".btn--hold");
+const current0El = document.getElementById("current--0");
+const current1El = document.getElementById("current--1");
 
 let scores, currentScore, activePlayer, playing;
 
-// INITIALIZATION or STARTING CONDITIONS
+// INITIALIZATION OR STARTING CONDITIONS
 const init = function () {
   scores = [0, 0];
   currentScore = 0;
@@ -26,11 +26,11 @@ const init = function () {
   current0El.textContent = 0;
   current1El.textContent = 0;
 
-  diceEl.classList.add('hidden');
-  player0El.classList.remove('player--winner');
-  player1El.classList.remove('player--winner');
-  player0El.classList.add('player--active');
-  player1El.classList.remove('player--active');
+  diceEl.classList.add("hidden");
+  player0El.classList.remove("player--winner");
+  player1El.classList.remove("player--winner");
+  player0El.classList.add("player--active");
+  player1El.classList.remove("player--active");
 };
 init();
 
@@ -38,18 +38,18 @@ const switchPlayer = function () {
   document.getElementById(`current--${activePlayer}`).textContent = 0;
   activePlayer = activePlayer === 0 ? 1 : 0;
   currentScore = 0;
-  player0El.classList.toggle('player--active');
-  player1El.classList.toggle('player--active');
+  player0El.classList.toggle("player--active");
+  player1El.classList.toggle("player--active");
 };
 
 //ROLLING THE DICE FUNCTIONALITY
-btnRoll.addEventListener('click', function () {
+btnRoll.addEventListener("click", function () {
   if (playing) {
     // 1. Generating a random dice roll
     const dice = Math.trunc(Math.random() * 6) + 1;
 
     // 2. Display dice
-    diceEl.classList.remove('hidden');
+    diceEl.classList.remove("hidden");
     diceEl.src = `dice-${dice}.png`;
 
     // 3. Check for rolled 1: if true, switch to next player
@@ -59,18 +59,18 @@ btnRoll.addEventListener('click', function () {
       document.getElementById(`current--${activePlayer}`).textContent =
         currentScore;
     } else {
-      //Switch to next player
+      // Switch to next player
       switchPlayer();
     }
   }
 });
 
 //HOLD BUTTON FUNCTION
-btnHold.addEventListener('click', function () {
+btnHold.addEventListener("click", function () {
   if (playing) {
     // 1. Add current score to active player's score
     scores[activePlayer] += currentScore;
-    //scores[1] = scores[1] + currentScore;
+    // or scores[1] = scores[1] + currentScore;
     document.getElementById(`score--${activePlayer}`).textContent =
       scores[activePlayer];
 
@@ -78,14 +78,14 @@ btnHold.addEventListener('click', function () {
     if (scores[activePlayer] >= 100) {
       // Finish game
       playing = false;
-      diceEl.classList.add('hidden');
+      diceEl.classList.add("hidden");
 
       document
         .querySelector(`.player--${activePlayer}`)
-        .classList.add('player--winner');
+        .classList.add("player--winner");
       document
         .querySelector(`.player--${activePlayer}`)
-        .classList.remove('player--active');
+        .classList.remove("player--active");
     } else {
       // Switch to the next player
       switchPlayer();
@@ -94,4 +94,4 @@ btnHold.addEventListener('click', function () {
 });
 
 // NEW GAME BUTTON FUNCTION
-btnNew.addEventListener('click', init);
+btnNew.addEventListener("click", init);
